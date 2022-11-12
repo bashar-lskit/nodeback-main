d03c671009ed0dc335475953985edf0856ee4a49
majharul <majharuljony163@gmail.com>
2022-11-12 10:19:27 +0600

    organized dashboard

--- client/app/components/Common/Footer/index.js
+++ client/app/components/Common/Footer/index.js
@@ -14,28 +14,6 @@ const Footer = () => {
         <div className='footer-copyright'>
           <span>© {new Date().getFullYear()} BCO || career-olympiad-bangladesh</span>
         </div>
-        <ul className='footer-social-item'>
-          <li>
-            <a href='/#facebook' rel='noreferrer noopener' target='_blank'>
-              <span className='facebook-icon' />
-            </a>
-          </li>
-          <li>
-            <a href='/#instagram' rel='noreferrer noopener' target='_blank'>
-              <span className='instagram-icon' />
-            </a>
-          </li>
-          <li>
-            <a href='/#pinterest' rel='noreferrer noopener' target='_blank'>
-              <span className='pinterest-icon' />
-            </a>
-          </li>
-          <li>
-            <a href='/#twitter' rel='noreferrer noopener' target='_blank'>
-              <span className='twitter-icon' />
-            </a>
-          </li>
-        </ul>
       </Container>
     </footer>
   );
